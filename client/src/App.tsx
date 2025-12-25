2fff66c7916d254e68b635834deb1a5159c36475
OntongGhebi <ghebiarmando@gmail.com>
2025-12-25 21:31:53 +0700

    setup for interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import MyProjects from "./pages/MyProjects";
+import View from "./pages/View";
+import Community from "./pages/Community";
+import Preview from "./pages/Preview";
+import Projects from "./pages/Projects";
+import Pricing from "./pages/Pricing";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/view/:projectId" element={<View />} />
+        <Route path="/preview/:projectId" element={<Preview />} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
