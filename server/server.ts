b1100d9c8341dbec86c0e3fba8d4b579adf22593
OntongGhebi <ghebiarmando@gmail.com>
2026-01-09 22:38:46 +0700

    setup auth feature with better-auth

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const port = 5000;
 
 const corsOptions = {
-  origin: process.env.TRUSTED_ORIGIN?.split(",") || [],
+  origin: process.env.TRUSTED_ORIGINS?.split(",") || [],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
